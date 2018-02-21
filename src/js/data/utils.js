9d6b7914806e83726ed03f85b0c3efcb62495514
termitavee <termitavee@gmail.com>
2018-02-21 10:06:56 +0100

    undo updates, if it works, dont cange it

--- src/js/data/utils.js
+++ src/js/data/utils.js
@@ -8,3 +8,21 @@ export function fileLog(d) {
   logFile.write('[' + Date() + ']' + util.format(d) + '\n');
   logStdout.write(util.format(d) + '\n');
 };
+
+export function loadJson(d) {
+  console.log(__dirname)
+  console.log(d)
+  const f = require(`./${d}`)
+
+  console.log(f)
+  return JSON.parse(f)
+};
+
+export function asyncLoad(p) {
+
+};
+
+export function throwJsError(err) {
+  shell.openExternal("https://stackoverflow.com/search?q=[js]+$(err)")
+};
+
